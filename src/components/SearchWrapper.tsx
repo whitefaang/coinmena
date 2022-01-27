c6abab02bd4eac045efd7bd41d53494323a382e9
shaikhrahil <smohammadrahil@gmail.com>
2022-01-28 04:37:19 +0530

    ♻️ refactor : new search wrapper with search debounce

---
+++ src/components/SearchWrapper.tsx
@@ -0,0 +1,27 @@
+import React, { ReactElement, useState } from 'react'
+import useDebounce from '../hooks/useDebounce'
+import Input from './atomic/Input'
+
+type Props = {
+  children: (searchKey: string) => ReactElement
+}
+
+function SearchWrapper({ children }: Props) {
+  const [searchKey, setSearchKey] = useState('')
+  const debouncedSearchKey = useDebounce(searchKey)
+  const render = () => children(debouncedSearchKey)
+  return (
+    <>
+      <Input
+        name="searchKey"
+        className="shadow-md sticky"
+        placeholder="Search .."
+        value={searchKey}
+        onChange={(e: any) => setSearchKey(e.target.value)}
+      />
+      {render()}
+    </>
+  )
+}
+
+export default SearchWrapper
